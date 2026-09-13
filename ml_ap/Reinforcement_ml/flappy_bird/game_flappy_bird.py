cf3ea870ff4be940fe398b3b2c33760319834116
Rahul Panigrahy <rahulpanigrahy650@gmail.com>
2026-09-13 16:29:38 +0530

    Add files via upload

---
+++ ml_ap/Reinforcement_ml/flappy_bird/game_flappy_bird.py
@@ -0,0 +1,26 @@
+import gymnasium as gym  
+import flappy_bird_gymnasium
+import pygame
+
+# creting our env
+env = gym.make("FlappyBird-v0",render_mode="human")
+state,info = env.reset ()
+done = False
+
+# initialize pyGame Keyboard
+pygame.init()
+
+screen = pygame.display.get_surface() # gym has already created a window
+
+while not done:
+    action =0 # default -> 0  is no flap & 1 is flap
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                action = 1 # flap
+    state,reward,done,truncated,info = env.step(action)
+    env.render()
+env.close
